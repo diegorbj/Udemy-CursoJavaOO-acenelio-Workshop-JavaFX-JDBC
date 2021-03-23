ab7bed987bf4c7acf8f8d6988fc8571caa9345b1
Diego Barros <diegorbj@outlook.com>
2021-03-23 15:07:18 -0300

    Fix bug on update Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -81,7 +81,7 @@ public class DepartmentFormController implements Initializable {
 
         ValidationException exception = new ValidationException("Validation error.");
 
-        obj.setId(Utils.tryParseToInt(txtId.getId()));
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
         if (txtName.getText() == null || txtName.getText().trim().equals("")) {
             exception.addError("name", "Field can´t be empty");
         }
